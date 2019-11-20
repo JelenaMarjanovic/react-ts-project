ec8eb6bf8f9c0b4655b537d1f1bdab06ca8b0313
Jelena Marjanovic <jelena.marjanovic.13@gmail.com>
2019-11-20 17:19:53 +0100

    Using TS Together with ReactJS - basic approach

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import CounterOutput from "./CounterOutput";
+
+// interface IAppProps {}
+
+interface IAppState {
+  counterValue: number;
+}
+
+class App extends React.Component<{}, IAppState> {
+  public state = { counterValue: 0 };
+
+  private decrementHandler = () =>
+    this.setState(previousState => {
+      return {
+        counterValue: previousState.counterValue - 1
+      };
+    });
+
+  private incrementHandler = () =>
+    this.setState(previousState => {
+      return {
+        counterValue: previousState.counterValue + 1
+      };
+    });
+
+  public render() {
+    return (
+      <div style={{ textAlign: "center" }}>
+        <CounterOutput counter={this.state.counterValue} />
+        <button onClick={this.decrementHandler}>Decrement</button>
+        <button onClick={this.incrementHandler}>Increment</button>
+      </div>
+    );
+  }
 }
 
 export default App;
